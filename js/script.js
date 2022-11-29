446fe07b1ecc95bbcf6396fc010f866a8a252b23
eusbeidy <eurimary@gmail.com>
2022-11-28 22:04:06 -0500

    se creo un archivo js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let minombre= prompt("ingresa tu nombre");
+console.log(minombre);
+
+let edad= prompt("edad");
+console.log(edad);
+
+let minombreYedad= minombre + ' ' + edad;
+document.write(minombreYedad);
+
