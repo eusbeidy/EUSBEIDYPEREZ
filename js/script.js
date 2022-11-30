794d00117aeeef7cef2bfe4b90e095689f5cc2ec
eusbeidy <eurimary@gmail.com>
2022-11-29 22:10:37 -0500

    se agrego contenido al file js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,35 @@
-let minombre= prompt("ingresa tu nombre");
-console.log(minombre);
+//let minombre= prompt("ingresa tu nombre");
+//console.log(minombre);
 
-let edad= prompt("edad");
-console.log(edad);
+//let edad= prompt("edad");
+//console.log(edad);
 
-let minombreYedad= minombre + ' ' + edad;
-document.write(minombreYedad);
+//let minombreYedad= minombre + ' ' + edad;
+//document.write(minombreYedad);
 
+//taller 
+/*let numero1 = parseInt(prompt('Ingresa el primer numero'));
+let numero2 = prompt('Ingresa el segundo numero');
+numero2 = parseInt(numero2);
+ 
+let resultado = numero1 + numero2;
+document.write('el resultado de la suma:', numero1,'+', numero2, '=', resultado)
+document.write('<br>')
+resultado = numero1 - numero2;
+document.write('el resultado de la resta:', numero1, '-', numero2, '=', resultado)
+document.write('<br>')
+resultado = numero1 * numero2;
+document.write('el resultado de la multiplicación:', numero1,'*', numero2, '=', resultado)
+document.write('<br>')
+resultado = numero1 / numero2;
+document.write( 'el resultado de la división:', numero1, '/', numero2, '=', resultado)
+document.write('<br>')*/
+
+//cadenas literales
+
+let numero1 = parseInt(prompt('Ingresa el primer numero'));
+let numero2 = prompt('Ingresa el segundo numero');
+numero2 = parseInt(numero2);
+
+let numero1Numero2 = `la suma de ${numero1} mas ${numero2} es = ${12}`;
+console.log(numero1Numero2);
