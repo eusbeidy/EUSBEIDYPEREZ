d69e259c8954b8a21ac02bacf707b569f95272c7
eusbeidy <eurimary@gmail.com>
2022-11-30 22:03:33 -0500

    seagrego contenido a js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+'use strict';
 //let minombre= prompt("ingresa tu nombre");
 //console.log(minombre);
 
@@ -27,9 +28,70 @@ document.write('<br>')*/
 
 //cadenas literales
 
-let numero1 = parseInt(prompt('Ingresa el primer numero'));
+/*let numero1 = parseInt(prompt('Ingresa el primer numero'));
 let numero2 = prompt('Ingresa el segundo numero');
 numero2 = parseInt(numero2);
 
 let numero1Numero2 = `la suma de ${numero1} mas ${numero2} es = ${12}`;
-console.log(numero1Numero2);
+console.log(numero1Numero2);*/
+
+//OPERADORES
+//modulo
+
+/*let nmr1 = parseInt(prompt('ingresa el primer numero'))
+let nmr2 = parseInt(prompt('ingresa el segundo numero'))
+ 
+nmr1++;
+nmr2--;
+let resultado = nmr1 * nmr2;
+document.write ('el resultado de', nmr1, '*', nmr2, '=', resultado);*/
+
+//operadores de comparacion
+ /*let variable1 = '10';
+ let variable2 = 10;
+
+ let resultado = variable1 == variable2;
+ resultado = variable1 === variable2;
+ resultado = variable1 != variable2;
+ resultado = variable1 = variable2;
+ resultado = variable1 > variable2;
+ resultado = variable1 < variable2;
+ resultado = variable1 >= variable2;
+ resultado = variable1 <= variable2;*/
+ 
+
+
+ //operadores logicos
+
+ /*let variable1 = 1
+ let variable2 = 2;
+ let resultado = variable1 === 1 && variable2 === 2;
+ resultado = variable1 === 1 || variable2 === 2;
+ document.write ('resultado', resultado); 
+ document.write ('resultado', !resultado); */
+
+ //TALLER
+ let edad = parseInt(prompt('Ingresa tu edad'));
+ let resultado = edad === 18;
+ document.write('su edad es ${edad}')
+ console.log(resultado)
+ document.write('<br>')
+
+ let nombre = prompt('Ingresa tu nombre');
+ let pepita = 'pepita'; 
+ resultado = nombre === pepita;
+ document.write('su nombre es ${nombre}');
+ console.log(resultado);
+
+ /*let numero =prompt('igresa un numero')
+ console.log(typeof numero);
+ numero = Number(numero)
+ document.write(numero)*/
+
+
+
+
+
+
+
+
